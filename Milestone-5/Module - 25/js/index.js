fdf46d9d32bbaa13c68918ad87088a1ebbca4c25
unknown <alamgirhossaindrubo@gmail.com>
2025-08-21 23:47:24 +0600

    Start events

---
+++ Milestone-5/Module - 25/js/index.js
@@ -0,0 +1,7 @@
+// on click event handler  
+
+const purpleBtn = document.getElementById('make-purple-btn');
+purpleBtn.onclick = function makePurple(){
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+}
