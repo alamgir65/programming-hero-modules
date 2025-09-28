d26a69275812872d7503af10a78d2965bbf33d1a
unknown <alamgirhossaindrubo@gmail.com>
2025-09-28 21:07:12 +0600

    start milestone 8

---
+++ React/router-state-management-explore/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './Conponents/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+
+      <Navbar></Navbar>
+
+      <h1 className='text-2xl font-bold'>Vite + React</h1>
+      <button className='btn btn-primary'>Button</button>
+      <button className="btn btn-warning">Warning</button>
+
+    </>
+  )
+}
+
+export default App
---
+++ React/router-state-management-explore/src/Conponents/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Navbar = () => {
+    const navLinks = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/blog", name: "Blog" },
+        { id: 5, path: "/contact", name: "Contact" },
+    ];
+
+    return (
+        <nav className='bg-slate-300 flex justify-between items-center px-10 py-3'>
+            <h1 className='text-3xl font-bold'>Alamgir</h1>
+            <ul className='flex gap-8 float-end'>
+                {
+                    navLinks.map(link => <li><a className='btn' href="">{link.name}</a></li>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
