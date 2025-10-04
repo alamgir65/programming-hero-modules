fd81d0821ec6dd82c3a0b1bf90569f74dd31909b
unknown <alamgirhossaindrubo@gmail.com>
2025-10-04 20:35:18 +0600

    Something about Route

---
+++ React/react-router/src/components/Laptops/Laptops.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Laptops = () => {
+    return (
+        <div>
+            <h2>This is Laptop Sections.</h2>
+        </div>
+    );
+};
+
+export default Laptops;
--- React/react-router/src/main.jsx
+++ React/react-router/src/main.jsx
@@ -3,8 +3,34 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+import Root from './components/Root/Root.jsx';
+import Home from './components/Root/Home.jsx';
+import Mobile from './components/Mobile/Mobile.jsx';
+import Laptops from './components/Laptops/Laptops.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {index: true, Component: Home},
+      {path: 'mobile' , Component: Mobile},
+      {path: 'laptops' , Component: Laptops}
+    ]
+  },
+  {
+    path : 'app',
+    Component : App
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
