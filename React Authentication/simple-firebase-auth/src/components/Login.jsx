49f0db0c224f97397987dadb56d9cc0b03013614
unknown <alamgirhossaindrubo@gmail.com>
2025-10-14 06:50:10 +0600

    Firebase google and github auth added

--- React Authentication/simple-firebase-auth/src/components/Login.jsx
+++ React Authentication/simple-firebase-auth/src/components/Login.jsx
@@ -1,20 +1,58 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import { auth } from '../firebase/firebase.init';
 
 const Login = () => {
+
+    const [user,setUser] = useState(null);
+
     const AuthProvider =new GoogleAuthProvider();
 
     const handleLoginWithGoogle = () => {
         signInWithPopup(auth, AuthProvider)
-            .then(res =>  console.log(res))
+            .then(res =>  {
+                console.log(res);
+                setUser(res.user);
+            })
             .catch(err => {
                 console.log(err);
             })
     }
+    const signOutHandler = () => {
+        signOut(auth)
+            .then(res => {
+                console.log(res);
+                setUser(null);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    const githubProvider = new GithubAuthProvider();
+    const signInGithubHandler = () => {
+        signInWithPopup(auth, githubProvider)
+            .then(res => {
+                console.log(res);
+                setUser(res.user);
+            })
+            .catch(err => console.log(err));
+    }
     return (
         <div>
-            <button onClick={handleLoginWithGoogle}>Sign in with google</button>
+            {
+                user? <button onClick={signOutHandler}>SignOut</button> : <>
+                    <button onClick={handleLoginWithGoogle}>Sign in with google</button>
+                    <button onClick={signInGithubHandler}>SignIn with Github</button>
+                </>
+            }
+            
+            
+            {
+                user && <div>
+                    <h1>{user.displayName}</h1>
+                    <p>{user.email}</p>
+                </div>
+            }
         </div>
     );
 };
